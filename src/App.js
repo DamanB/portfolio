88805c6743a39a41e92163ec7d5ece3152603a5f
DamanB <damanbharaj@gmail.com>
2021-05-03 16:22:23 -0400

    restructured project directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Navbar from './components/Navbar.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
+      <div className="app-content">
+        <h1>Hello World!</h1>        
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../styles/navbar.css'
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="nav-links">
+                <a href="/">Home</a>
+                <a href="/">Resume</a>
+                <a href="/">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
