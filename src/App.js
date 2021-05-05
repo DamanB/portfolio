160d6e25116e8468a23824745bce170eb6b22f00
DamanB <damanbharaj@gmail.com>
2021-05-05 17:26:59 -0400

    added new components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import './styles/app.css'
 import Navbar from './components/Navbar.js'
 import Hero from './components/Hero.js'
+import AboutMe from './components/AboutMe.js'
+import Skills from './components/Skills.js'
+
+// import IntroArt from './assets/img/intro_art.png'
+import Spacer from './components/Spacer.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="app-content">
-        <Hero />        
+      <Hero />
+
+      <div className="intro-container">
+        <Spacer />
+        <div className="intro-content">
+          <div>
+            <AboutMe />
+            <Skills />
+          </div>
+          <div class="intro-icon-container">
+            <span class="material-icons-outlined">person</span>
+            <span class="">&lt; hello world! &gt;</span>
+          </div>
+        </div>
+        <Spacer />
+      </div>
+
+      <div className="software-projects-container">
+        
       </div>
     </div>
   );
