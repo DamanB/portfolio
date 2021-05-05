afeddf1bbcd949175ce5a0c8b1c4674297050e97
DamanB <damanbharaj@gmail.com>
2021-05-05 17:25:57 -0400

    created aboutme component and styles

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import '../styles/aboutme.css'
+
+const AboutMe = () => {
+    return ( 
+        <div className="aboutMe">
+            <h2 className="ft-medium">About <span>Me</span></h2>
+            <p className="ft-bold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores cumque debitis ad omnis doloremque, officiis tempora natus laudantium enim, dicta delectus quam? Necessitatibus, temporibus similique. Sapiente, consequuntur architecto. Fuga, modi!</p>
+            <p className="ft-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veritatis ducimus consequuntur eos, excepturi similique! Dolores dolore explicabo facere modi quia at ipsa aperiam repudiandae alias ipsum eligendi veniam eveniet voluptas reprehenderit nobis sunt provident, iste ab laborum ut, suscipit culpa quasi minima facilis. Ipsam eius commodi corrupti atque laborum facilis perspiciatis ratione cupiditate libero, earum ducimus aut vitae temporibus possimus rem, exercitationem necessitatibus labore, dolorem dicta vero molestiae! Sequi minima doloremque molestiae repellendus nihil.</p>
+        </div>
+     );
+}
+ 
+export default AboutMe;
