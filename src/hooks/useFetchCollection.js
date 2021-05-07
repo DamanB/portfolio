44cf8ac4a68ecd8d735566c1e4f48014eeb4ef3c
DamanB <damanbharaj@gmail.com>
2021-05-06 21:25:43 -0400

    added firestore dependencies

---
+++ src/hooks/useFetchCollection.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import db from '../utility/firebase.config.js';
+
+const useFetchCollection = (collection) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fetch = async() => {
+        try{
+            setIsPending(true)
+            const response = await db.collection(collection).get();
+            //Need to add unique doc id to document
+            setData(response.docs.map(doc => {
+                return {...doc.data(), id: doc.id}
+            }));
+        }
+        catch(err){
+            setError("Unable to fetch from database")
+        }
+        finally {
+            setIsPending(false)
+        }
+    }
+
+    useEffect(() => {
+        const abortCont = new AbortController();
+        fetch()
+        return () => abortCont.abort();
+    },[collection]);
+
+    return { data, isPending, error };
+}
+
+export default useFetchCollection
