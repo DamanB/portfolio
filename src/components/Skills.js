8c4273b27f8a70d6cdf3cbaee5823148504f545f
DamanB <damanbharaj@gmail.com>
2021-05-08 01:08:20 -0400

    updated skills to parse from array

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,22 +1,24 @@
 import '../styles/skills.css'
+import { useState } from 'react'
 import SkillPill from './SkillPill.js'
 
 const Skills = () => {
-    return ( 
+
+    const [skills] = useState(["Java", "JavaScript", "HTML", "CSS", "Vue.js", "React", "jQuery", "Bootstrap 4", "Relational Databases", "Responsive Design", "Version Control"]);
+
+    return (
         <div className="skills">
             <h2>Noteworthy Skills</h2>
             <div className="skillPills">
-                <SkillPill skill="Java"/>
-                <SkillPill skill="JavaScript"/>
-                <SkillPill skill="CSS"/>
-                <SkillPill skill="HTML"/>
-                <SkillPill skill="Bootstrap 4"/>
-                <SkillPill skill="Vue.js"/>
-                <SkillPill skill="Relational Databases"/>
-                <SkillPill skill="Responsive Design"/>
+                {(
+                    skills.map((skill, index) => (
+                        <SkillPill skill={skill} key={index} />
+
+                    ))
+                )}
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Skills;
