7756d1215f1a93ed7b3d3179737d724e22006da7
DamanB <damanbharaj@gmail.com>
2021-05-19 16:49:56 -0400

    modified space around containers to be consistent

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,30 +2,38 @@ import '../styles/home.css'
 import Hero from '../components/Hero.js'
 import AboutMe from '../components/AboutMe.js'
 import Skills from '../components/Skills.js'
+import Contact from '../components/Contact.js'
 import SoftwareProjects from '../components/SoftwareProjects'
 import Spacer from '../components/Spacer.js'
 
 const HomePage = () => {
-    return ( <div className="homepage">
-        <Hero />
-              <div className="intro-container">
-                <Spacer />
-                <AboutMe />
-                <Spacer />
-              </div>
+  return (<div className="homepage">
+    <Hero />
+    
+    <div className="intro-container">
+      <Spacer />
+      <AboutMe />
+      <Spacer />
+    </div>
 
-              <div className="skills-container">
-                <Spacer />
-                <Skills />
-                <Spacer />
-              </div>
+    <div className="home-container">
+      <Spacer />
+      <Skills />
+      <Spacer />
+    </div>
 
-              <div className="software-projects-container">
-                <Spacer />
-                <SoftwareProjects />
-                <Spacer />
-              </div>
-    </div> );
+    <div className="home-container">
+      <Spacer />
+      <Contact />
+      <Spacer />
+    </div>
+
+    <div className="software-projects-container">
+      <Spacer />
+      <SoftwareProjects />
+      <Spacer />
+    </div>
+  </div>);
 }
- 
+
 export default HomePage;
